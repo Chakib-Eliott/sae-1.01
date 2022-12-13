b1109a2ad5da32206a1fa537d83b0b33af9dead2
Eliott-B <aquafirechaine@gmail.com>
2022-12-13 10:54:56 +0100

    Ajout des bots

--- terminal.py
+++ terminal.py
@@ -9,6 +9,7 @@ Ce jeu est développé dans le cadre d'une SAé (un projet) avec l'IUT de Véliz
 import diamants as D  # Les fonction qui permettent de faire fonctionner le jeu
 import os  # Natif à Python
 import time  # Natif à Python
+from random import randint  # Natif à Python
 
 print("INITIALISATION DE LA PARTIE ...")
 print()
@@ -18,6 +19,14 @@ inputNombreJoueurs = 0
 while inputNombreJoueurs not in list(range(3,9)):  # Bloque le choix du joueur jusqu'à que le nombre rentré soit correct.
     try:
         inputNombreJoueurs = int(input("Choisir le nombre de joueurs (3 à 8) : "))
+    except ValueError:
+        print("Entrez un entier !")
+
+# Choix entre joueur et bot (0: bot, 1: joueur).
+choixJB = -1
+while choixJB not in list(range(2)):  # Bloque le choix du joueur jusqu'à que le nombre rentré soit correct.
+    try:
+        choixJB = int(input("Choisir le mode de jeu (0 = bot, 1 = joueur) : "))
     except ValueError:
         print("Entrez un entier !")
 
@@ -62,7 +71,7 @@ def affichageCourant(instruction = '') -> None:
     print("\n"*2)
 
 
-# Boucle du jeu
+# Boucle du jeu.
 while Jeu.manchesRestants > 0:
     if Jeu.joueursRestants == 0:
         Jeu.changementManche()
@@ -71,7 +80,8 @@ while Jeu.manchesRestants > 0:
         for i in Jeu.joueurs.items():
             clear()
             affichageCourant("Choix des joueurs...")
-            if i[1][2] == 0:  # Si le joueur n'est pas sortie.
+            # Si le joueur n'est pas sortie et qu'on joue avec des bots sinon il vérifie que c'est nous.
+            if (i[1][2]==0 and choixJB==1) or i[0]==1:
                 print("Tour du joueur :", i[0])  # i[0] = joueur jouant
                 caras = Jeu.caracteristiquesJoueur(i[0])
                 print("Diamants en votre possession :", caras[3])  # Nombre de diamants pendant l'expédition
@@ -93,11 +103,32 @@ while Jeu.manchesRestants > 0:
                     print()
                     print("Vous avez choisi de rester dans le jeu !")
                 time.sleep(1)
+            elif choixJB == 0:
+                print("Tour du joueur :", i[0])  # i[0] = joueur jouant
+                caras1 = Jeu.caracteristiquesJoueur(i[0])
+                print("Diamants en sa possession :", caras1[3])  # Nombre de diamants pendant l'expédition
+                caras = Jeu.caracteristiquesJoueur(1)
+                print("Diamants en votre possession :", caras[3])  # Nombre de diamants pendant l'expédition
+                print("Relique(s) en votre possession :", caras[1])
+                print("Nombre de diamants dans votre coffre :", caras[0])  # Nombre de diamants dans le coffre
+                print('\n'*2)
+                choix = randint(0,1)  # Choisi entre sortir et rester de façon aléatoire.
+                if choix == 1:
+                    Jeu.jouer(i[0],1)
+                    print()
+                    print("Le joueur a décidé de quitter ce tour !")
+                    sortis.append(i[0])  # Ajout du joueur dans la liste des joueurs sorties.
+                else:
+                    # Aucune intéraction nécessaire, le jeu continue pour ces joueurs.
+                    print()
+                    print("Le joueur a décidé de rester dans le jeu !")
+                time.sleep(2)
         Jeu.sortie(sortis)  # Vérifie que la manche n'est pas fini et réparti le trésor aux joueurs sorties.
     else:
         print("Un obstacle surgit ! Les joueurs restants dans la grotte fuit ! (L'obstacle était", Jeu.tapis[-1][0],")")
         time.sleep(5)
         Jeu.changementManche()
+
 
 clear()
 print("Fin de la partie.\nLe classement est :")
