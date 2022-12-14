b7a4c71bd297f73ca7bbb851c13474f43113ae2f
444chak <chakibouali@chak.tech>
2022-12-14 16:47:19 +0100

    Avancement graphique
    Co-authored-by: Eliott <Eliott-B@users.noreply.github.com>

--- graphique.py
+++ graphique.py
@@ -5,7 +5,6 @@ AUTEURS : Eliott & Chakib
 """
 import diamants # Les fonction qui permettent de faire fonctionner le jeu
 from tkinter import *
-from PIL import ImageTk, Image  # Natif à Python
 import tkinter.font as TkFont
 
 WIDTH="1000"
@@ -15,54 +14,67 @@ root = Tk()
 root.title('Diamants by Eliott & Chakib')
 root.geometry(f"{WIDTH}x{HEIGHT}")
 root.resizable(width=False, height=False)
-root.iconphoto(False, PhotoImage('asset/diamants_logo.png'))
+DIAMANTIMAGE = PhotoImage(file='./asset/diamants_logo.png')
+root.iconphoto(False, DIAMANTIMAGE)
 root.configure(bg=BACKGROUND)
 
 
-def plusplayer():
-    value = nbPlayers.get()
-    if int(value) < 8:
-        nbPlayers.delete(0,END)
-        nbPlayers.insert(0,str(int(value)+1))
-
-def minusplayer():
-    value = nbPlayers.get()
-    if int(value) > 3:
-        nbPlayers.delete(0,END)
-        nbPlayers.insert(0,str(int(value)-1))
 
 
-# Logo diamants
-diamant_image = PhotoImage(file='asset/diamants_logo.png')
-diamant = Label(image=diamant_image)
-diamant.configure(bg=BACKGROUND)  # Met la couleur du fond en fond de l'image PNG
-diamant.place(x=0, y=0)
+def accueil():
+    def plusplayer():
+        value = nbPlayers.get()
+        if int(value) < 8:
+            nbPlayers.delete(0,END)
+            nbPlayers.insert(0,str(int(value)+1))
 
-# Choix bot ou non
-bots = Checkbutton(root, text = "Bots", height = 2, width = 10, bg=BACKGROUND, activebackground=BACKGROUND)
-bots.place(x=750, y=300)
+    def minusplayer():
+        value = nbPlayers.get()
+        if int(value) > 3:
+            nbPlayers.delete(0,END)
+            nbPlayers.insert(0,str(int(value)-1))
+    # Logo diamants
+    diamant = Label(image=DIAMANTIMAGE)
+    diamant.configure(bg=BACKGROUND)  # Met la couleur du fond en fond de l'image PNG
+    diamant.place(x=0, y=0)
 
-# Label nombre joueurs
-labelnbjoueurs = Label(root, text='Nombre de joueurs', bg=BACKGROUND)
-labelnbjoueurs.place(x=115, y=275)
+    # Choix bot ou non
+    bots = Checkbutton(root, text = "Bots", height = 2, width = 10, bg=BACKGROUND, activebackground=BACKGROUND)
+    bots.place(x=750, y=300)
 
-# Bouton pour diminuer le nombre de joueur
-reduire = Button(root, text ="-", command = minusplayer)
-reduire.place(x= 113, y= 300)
+    # Label nombre joueurs
+    labelnbjoueurs = Label(root, text='Nombre de joueurs', bg=BACKGROUND)
+    labelnbjoueurs.place(x=115, y=275)
 
-# Input du nombre de joueur
-cmd = root.register(lambda s: not s or (s.isdigit() and  int(s) >= 3 and int(s)< 9))
-nbPlayers = Entry(root, validate="key", vcmd=(cmd, "%P"), width=5)
-nbPlayers.place(x= 150, y= 300)
-nbPlayers.insert(0, "3")
-nbPlayers.bind("<Return>")  
+    # Bouton pour diminuer le nombre de joueur
+    reduire = Button(root, text ="-", command = minusplayer)
+    reduire.place(x= 113, y= 300)
 
-# Bouton pour augmenter le nombre de joueur
-augmenter = Button(root, text ="+", command = plusplayer)
-augmenter.place(x=200, y= 300)
+    # Input du nombre de joueur
+    cmd = root.register(lambda s: not s or (s.isdigit() and  int(s) >= 3 and int(s)< 9))
+    nbPlayers = Entry(root, validate="key", vcmd=(cmd, "%P"), width=5)
+    nbPlayers.place(x= 150, y= 300)
+    nbPlayers.insert(0, "3")
+    nbPlayers.bind("<Return>")  
 
-jouer_text = TkFont.Font(size=18)
-jouer = Button(root, text ="JOUER!", font=jouer_text, activebackground='#89e37f')
-jouer.place(x=440, y= 300)
+    # Bouton pour augmenter le nombre de joueur
+    augmenter = Button(root, text ="+", command = plusplayer)
+    augmenter.place(x=200, y= 300)
+
+    jouer_text = TkFont.Font(size=18)
+    jouer = Button(root, text ="JOUER!", font=jouer_text, activebackground='#89e37f', command=Vider)
+    jouer.place(x=440, y= 300)
+
+def Jouer():
+    pass
+
+def Vider():
+    for widget in root.winfo_children():
+        widget.destroy()
+
+accueil()
+
+
+
 
 root.mainloop()
